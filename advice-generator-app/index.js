3a11dee0076bf447e4d1bf5f10b640fdf93a38d2
techtycho <omarahmedabdelhakim@gmail.com>
2022-07-06 01:14:49 +0200

    Stop the Refresh Button from generating the same advice

--- advice-generator-app/index.js
+++ advice-generator-app/index.js
@@ -1,17 +1,41 @@
 const btnRefresh = document.getElementById("refresh");
+const id = document.getElementById("id");
+const advice = document.getElementById("text");
 
-async function fetchData() {
-  const res = await fetch("https://api.adviceslip.com/advice");
+const defaultUrl = "https://api.adviceslip.com/advice";
+
+let i = 0;
+
+async function fetchData(url) {
+  const res = await fetch(url);
   const data = res.json();
   return data;
 }
 
-function getAdvice() {
-  fetchData().then((data) => {
-    document.getElementById("id").textContent = data.slip.id;
-    document.getElementById("text").textContent = data.slip.advice;
+function getAdvice(url) {
+  fetchData(url).then((res) => {
+    const data = res.slip;
+    const currentId = Number.parseInt(id.textContent);
+
+    if (data.id + i === currentId) {
+      const incremented = Number.parseInt(id.textContent) + 1;
+
+      fetchData(`https://api.adviceslip.com/advice/${incremented}`).then(
+        (_data) => {
+          i++;
+
+          id.textContent = incremented;
+          advice.textContent = _data.slip.advice;
+        }
+      );
+    } else {
+      i = 0;
+
+      id.textContent = data.id;
+      advice.textContent = data.advice;
+    }
   });
 }
 
-getAdvice();
-btnRefresh?.addEventListener("click", getAdvice);
+getAdvice(defaultUrl);
+btnRefresh?.addEventListener("click", () => getAdvice(defaultUrl));
